9b2186badc2a092421cc6958b902095faa12749e
kawadjou <amekoujc@gmail.com>
2019-02-22 15:03:47 +0100

    Supermarket test: renommage de Appletest, création des premiers unit tests

--- src/test/java/fr/esiea/SupermarketTest.java
+++ src/test/java/fr/esiea/SupermarketTest.java
@@ -1,31 +1,38 @@
 package fr.esiea;
 
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class AppleTest {
+public class SupermarketTest {
 
     @Test
     public void testSomething() {
         SupermarketCatalog catalog = new FakeCatalog();
+
         Product toothbrush = new Product("toothbrush", ProductUnit.Each);
         catalog.addProduct(toothbrush, 0.99);
+        Assertions.assertThat(catalog.getUnitPrice(toothbrush)).as("toothbrush price").isEqualTo(0.99);
+
         Product apples = new Product("apples", ProductUnit.Kilo);
         catalog.addProduct(apples, 1.99);
+        Assertions.assertThat(catalog.getUnitPrice(apples)).as("apple price").isEqualTo(1.99);
 
         ShoppingCart cart = new ShoppingCart();
+        Assertions.assertThat(cart.getItems().size()).as("cart size").isEqualTo(0);
+
         cart.addItemQuantity(apples, 2.5);
+        Assertions.assertThat(cart.getItems().size()).as("cart size").isEqualTo(1);
 
         Teller teller = new Teller(catalog);
         teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0);
 
         Receipt receipt = teller.checksOutArticlesFrom(cart);
+        Assertions.assertThat(receipt.getTotalPrice()).as("cart price").isEqualTo(2.5*1.99);
 
-
-        // Todo: complete this test
-        double bill = 2.5*1.99;
-
-        if(receipt.getTotalPrice() == bill){
-            System.out.println( "correct" );
-        }
     }
+
+
+
+
 }
