58ca4b34a1c14b21651f78fa05b7d27a635be815
tinmar <martin.lavallart@gmail.com>
2019-02-26 17:27:57 +0100

    Ajout des fonctions de test correspondantes aux offres.
        threeForTwoTest()
        tenPercentDiscountTest()
        twoForAmountTest()
        FiveForAmountTest()

--- src/test/java/fr/esiea/supermarket/model/SupermarketTest.java
+++ src/test/java/fr/esiea/supermarket/model/SupermarketTest.java
@@ -32,6 +32,93 @@ public class SupermarketTest {
 
     }
 
+    @Test
+    public void threeForTwoTest() {
+
+        SupermarketCatalog catalog = new FakeCatalog();
+
+        Product toothbrush = new Product("toothbrush", ProductUnit.Each);
+        catalog.addProduct(toothbrush, 0.99);
+        Assertions.assertThat(catalog.getUnitPrice(toothbrush)).as("toothbrush price").isEqualTo(0.99);
+
+        ShoppingCart cart = new ShoppingCart();
+        Assertions.assertThat(cart.getItems().size()).as("cart size").isEqualTo(0);
+        cart.addItemQuantity(toothbrush, 3.0);
+        Assertions.assertThat(cart.getItems().size()).as("cart size").isEqualTo(1);
+
+        Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(SpecialOfferType.ThreeForTwo, toothbrush,0);
+
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+        Assertions.assertThat(receipt.getTotalPrice()).as("cart price").isEqualTo(0.99 * 2);
+
+    }
+
+    @Test
+    public void tenPercentDiscountTest() {
+
+        SupermarketCatalog catalog = new FakeCatalog();
+
+        Product toothbrush = new Product("toothbrush", ProductUnit.Each);
+        catalog.addProduct(toothbrush, 0.99);
+
+        Product apples = new Product("apples", ProductUnit.Kilo);
+        catalog.addProduct(apples, 1.99);
+
+        ShoppingCart cart = new ShoppingCart();
+
+        cart.addItemQuantity(apples, 3);
+        cart.addItemQuantity(toothbrush, 3);
+
+        Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0);
+        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, apples, 10.0);
+
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        Assertions.assertThat(receipt.getTotalPrice()).as("cart price").isEqualTo(((3*1.99)-(3*1.99*0.1))+((3*0.99)-(3*0.99*0.1)));
+
+    }
+
+    @Test
+    public void twoForAmountTest() {
+
+        SupermarketCatalog catalog = new FakeCatalog();
+        Product toothpaste = new Product("toothpaste", ProductUnit.Each);
+        catalog.addProduct(toothpaste,2);
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItemQuantity(toothpaste, 2);
+
+        Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(SpecialOfferType.TwoForAmount, toothpaste,3);
+
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        Assertions.assertThat(receipt.getTotalPrice()).as("cart price").isEqualTo(3);
+
+
+    }
+
+    @Test
+    public void fiveForAmountTest() {
+
+        SupermarketCatalog catalog = new FakeCatalog();
+        Product toothpaste = new Product("toothpaste", ProductUnit.Each);
+        catalog.addProduct(toothpaste,2);
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItemQuantity(toothpaste, 6);
+
+        Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(SpecialOfferType.FiveForAmount, toothpaste,7);
+
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        Assertions.assertThat(receipt.getTotalPrice()).as("cart price").isEqualTo(9);
+
+
+    }
 
 
 
